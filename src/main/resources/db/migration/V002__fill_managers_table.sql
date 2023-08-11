d830166fe7f3ef679cd6be5d2ac7714849ce68e7
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-08-12 02:49:36 +0300

    Filling Managers table

---
+++ src/main/resources/db/migration/V002__fill_managers_table.sql
@@ -0,0 +1,6 @@
+insert into public.managers (name, email) values
+('Phebe Lehner', 'lehner@company.com'),
+( 'Gilbert McLaughlin', 'mc_laughlin@company.com'),
+('Christin Stracke Jr.', 'stracke_jr@company.com'),
+( 'Brenda Lang', 'lehner@company.com'),
+('Jesse McGlynn', 'mc_glynn@company.com');

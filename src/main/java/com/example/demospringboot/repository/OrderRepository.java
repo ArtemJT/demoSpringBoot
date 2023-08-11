54f27a9f51ec7a3df92ee4ee1a8758d53501f4db
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-08-12 02:51:25 +0300

    Added findAllByCustomer and findAllByManager methods

--- src/main/java/com/example/demospringboot/repository/OrderRepository.java
+++ src/main/java/com/example/demospringboot/repository/OrderRepository.java
@@ -1,12 +1,20 @@
 package com.example.demospringboot.repository;
 
+import com.example.demospringboot.domain.Customer;
+import com.example.demospringboot.domain.Manager;
 import com.example.demospringboot.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Artem Kovalov on 09.08.2023
  */
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    List<Order> findAllByCustomer(Customer customer);
+
+    List<Order> findAllByManager(Manager manager);
 }

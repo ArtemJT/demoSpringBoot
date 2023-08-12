6925d4dd9153519ba65cff3a2ea5e718707b3b0b
Artem <105941754+ArtemJT@users.noreply.github.com>
2023-08-12 16:00:31 +0300

    Removed not used methods

--- src/main/java/com/example/demospringboot/util/mapper/BookingMapper.java
+++ src/main/java/com/example/demospringboot/util/mapper/BookingMapper.java
@@ -1,12 +1,9 @@
 package com.example.demospringboot.util.mapper;
 
 import com.example.demospringboot.domain.Booking;
-import com.example.demospringboot.dto.BookingDto;
 import com.example.demospringboot.dto.BookingFullDto;
 import com.example.demospringboot.dto.BookingReadDto;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -19,14 +16,8 @@ public interface BookingMapper {
     BookingMapper INSTANCE = Mappers.getMapper(BookingMapper.class);
 
     BookingFullDto toFullDto(Booking booking);
+
     BookingReadDto toReadDto(Booking booking);
-
-    BookingDto toDto(Booking booking);
-
-    @InheritInverseConfiguration
-    @Mapping(target = "creationDate", ignore = true)
-    @Mapping(target = "status", ignore = true)
-    Booking toEntity(BookingDto bookingDto);
 
     List<BookingReadDto> toReadDtoCol(List<Booking> bookingList);
 
